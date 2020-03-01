8c77df9d31e49f67c651e5ad2fb0ddf05135650f
tolgatuna <tolga.tuna@hotmail.com>
2020-03-01 22:59:16 +0000

    Report API Project is initialized

---
+++ src/test/java/com/financialhouse/assignment/reporting/ReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialhouse.assignment.reporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
